73ae554a556008be97ee238d435f63c5a7b26f87
Matias Wargelin <matias.wargelin@csc.fi>
2024-02-13 13:55:42 +0000

    TPASPKT-1280 Expose __version__
    Expose version number for easy retrieval at runtime. This is helpful for
    creating premis agent for mets-builder, which needs the version number
    metadata.

--- mets_builder/__init__.py
+++ mets_builder/__init__.py
@@ -5,6 +5,12 @@ from mets_builder.mets import (METS, AgentRole, AgentType, MetsProfile,
                                MetsRecordStatus)
 from mets_builder.structural_map import StructuralMap, StructuralMapDiv
 
+try:
+    from ._version import version as __version__
+except ImportError:
+    # Package not installed, provide something
+    __version__ = "N/A"
+
 __all__ = [
     "DigitalObject", "DigitalObjectStream", "FileGroup", "FileReferences",
     "METS", "AgentRole", "AgentType", "MetsProfile", "MetsRecordStatus",
