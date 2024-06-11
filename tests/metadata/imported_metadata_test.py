1a9af8452839f4c0c8a4db82cbb3c35984b84805
Kristian Wallgren <kwallgre@csc.fi>
2024-06-11 15:13:13 +0300

    TPASPKT-1344 Add tests for importing metadata from string

--- tests/metadata/imported_metadata_test.py
+++ tests/metadata/imported_metadata_test.py
@@ -22,8 +22,9 @@ def test_importing_metadata_with_invalid_data_path():
     )
 
 
-def test_serializing_imported_metadata():
-    """Test that imported metadata is serialized correctly."""
+def test_serializing_imported_metadata_with_data_path():
+    """Test that imported metadata is serialized correctly using data path.
+    """
     data = metadata.ImportedMetadata(
         data_path=Path("tests/data/imported_metadata.xml"),
         metadata_type=metadata.MetadataType.DESCRIPTIVE,
@@ -42,3 +43,58 @@ def test_serializing_imported_metadata():
     assert len(root_element) == 2
     assert root_element[0].tag == "sub1"
     assert root_element[1].tag == "sub2"
+
+
+def test_serializing_imported_metadata_with_data_string():
+    """Test that imported metadata is serialized correctly using a data string.
+    """
+    data = metadata.ImportedMetadata(
+        data_string="<root><sub1></sub1><sub2></sub2></root>",
+        metadata_type=metadata.MetadataType.DESCRIPTIVE,
+        metadata_format=metadata.MetadataFormat.OTHER,
+        other_format="PAS-special",
+        format_version="1.0"
+    )
+    root_element = data.to_xml_element_tree()
+
+    # The xml file content is
+    # <root>
+    #   <sub1></sub1>
+    #   <sub2></sub2>
+    # </root>
+    assert root_element.tag == "root"
+    assert len(root_element) == 2
+    assert root_element[0].tag == "sub1"
+    assert root_element[1].tag == "sub2"
+
+
+def test_importing_metadata_without_data_path_or_data_string():
+    """Test that giving ImportedMetadata neither data path or data string
+    results in error.
+    """
+    with pytest.raises(ValueError) as error:
+        metadata.ImportedMetadata(
+            metadata_type=metadata.MetadataType.DESCRIPTIVE,
+            metadata_format=metadata.MetadataFormat.DC,
+            format_version="1.0"
+        )
+    assert str(error.value) == (
+        "No data path or data string given"
+    )
+
+
+def test_importing_metadata_with_data_path_and_data_string():
+    """Test that giving ImportedMetadata both data path and data string results
+    in error.
+    """
+    with pytest.raises(ValueError) as error:
+        metadata.ImportedMetadata(
+            data_path=Path("/test/path"),
+            data_string="<root><sub1></sub1><sub2></sub2></root>",
+            metadata_type=metadata.MetadataType.DESCRIPTIVE,
+            metadata_format=metadata.MetadataFormat.DC,
+            format_version="1.0"
+        )
+    assert str(error.value) == (
+        "Both data path and data string given."
+    )
