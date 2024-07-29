b07576470874ce330f021490a777d88c946b8bd5
Janne Pulkkinen <janne.pulkkinen@csc.fi>
2024-07-29 14:22:27 +0000

    Make example code test more robust
    The test to run example code in documentation had a few issues:
    
    * `os.chdir` was used to change global state. If test failed, this would
      also affect later tests. Use `monkeypatch` fixture to ensure changes
      only affect the test.
    * Tests were executed in the original directory and consequently
      modified files inside it. This would cause unwanted files to be
      created if test failed. Fix this by copying files to a temporary
      location using the `tmpdir` fixture and running the script there.

--- tests/example_code_test.py
+++ tests/example_code_test.py
@@ -1,10 +1,19 @@
 import runpy
-import os
+import shutil
+from pathlib import Path
 
 
-def test_example_code():
+def test_example_code(monkeypatch, tmpdir):
     """Test that example code runs without error."""
-    os.chdir("doc/source/")
+    # Copy doc source files to a temporary location to avoid accidentally
+    # modifying the original directory if the test crashes for some reason
+    doc_path = Path(tmpdir / "doc" / "source")
+
+    doc_path.parent.mkdir(parents=True)
+    shutil.copytree("doc/source", doc_path)
+
+    monkeypatch.chdir(doc_path)
     runpy.run_path("example_code.py")
-    os.chdir("../..")
-    os.remove("doc/source/mets.xml")
+
+    # 'mets.xml' was created
+    assert (doc_path / "mets.xml").is_file()
