b2815b30e663c6e3017358eb20487078d807a95f
Kristian Wallgren <kwallgre@csc.fi>
2024-08-15 15:44:34 +0300

    TPASPKT-1351 Improve uuid module documentation

--- mets_builder/uuid.py
+++ mets_builder/uuid.py
@@ -1,12 +1,27 @@
-"""Module for uuid utilities."""
+"""Module for generating UUIDs"""
 from uuid import uuid4
 
 
 def uuid() -> str:
-    """Generate a random uuid"""
+    """Generate a random uuid
+
+    This function is a thin wrapper for the 'uuid4' function the standard
+    library 'uuid' module. This function exist so we don't have to use both the
+    standard library 'uuid' module and this module both regular UUIDs and UUIDs
+    prefixed with an underscore are needed
+
+    :returns: UUID
+    """
     return f"{uuid4()}"
 
 
 def underscore_uuid() -> str:
-    """Generate a random uuid with prefixed underscore"""
+    """Generate a random uuid with prefixed underscore
+
+    Some identifier fields in METS schema start with an underscore. This
+    function is a thin wrapper for the 'uuid4' function the standard library
+    'uuid' module which  prefixed with an underscore to the UUID.
+
+    :returns: UUID prefix with an underscore
+    """
     return f"_{uuid4()}"
