d8f90cd09e3df783fc699e6fde0231002a584082
Henri Johansson <hjohanss@csc.fi>
2025-07-24 12:30:58 +0000

    TPASPKT-1520 Refactor SonarQubes codesmell from variable "format"

--- mets_builder/metadata/mets_metadata.py
+++ mets_builder/metadata/mets_metadata.py
@@ -5,19 +5,19 @@ class MetsMetadataType:
     def __init__(
         self,
         uri: str = None,
-        format: mdf = None,
+        mdt_format: mdf = None,
         version: str = None,
         other_format: str = None,
     ):
-        assert None not in [uri, format, version]
+        assert None not in [uri, mdt_format, version]
         if other_format:
-            format = mdf.OTHER
-        if format == mdf.OTHER and not other_format:
+            mdt_format = mdf.OTHER
+        if mdt_format == mdf.OTHER and not other_format:
             raise ValueError(
                 "'metadata_format' is 'OTHER' but 'other_format' is "
                 "not given.")
         self.uri = uri
-        self.format = format
+        self.mdt_format = mdt_format
         self.version = version
         self.other_format = other_format
 
@@ -26,98 +26,98 @@ class MetsMetadataType:
 METS_MDTYPES_LIST = [
     MetsMetadataType(
         uri="http://purl.org/dc/elements/1.1",
-        format=mdf.DC,
+        mdt_format=mdf.DC,
         version="2008"
     ),
     MetsMetadataType(
         uri="http://www.loc.gov/MARC21/slim",
-        format=mdf.MARC,
+        mdt_format=mdf.MARC,
         version="marcxml=1.2; marc=marc21",
     ),
     MetsMetadataType(
         uri="http://www.loc.gov/mods/v3",
-        format=mdf.MODS,
+        mdt_format=mdf.MODS,
         version="3.8"
     ),
     MetsMetadataType(
         uri="urn:isbn:1-931666-22-9",
-        format=mdf.EAD,
+        mdt_format=mdf.EAD,
         version="2002"
     ),
     MetsMetadataType(
         uri="http://ead3.archivists.org/schema",
-        format=mdf.OTHER,
+        mdt_format=mdf.OTHER,
         other_format="EAD3",
         version="1.1.1",
     ),
     MetsMetadataType(
         uri="urn:isbn:1-931666-33-4",
-        format=mdf.EAC_CPF,
+        mdt_format=mdf.EAC_CPF,
         version="2010_revised",
     ),
     MetsMetadataType(
         uri="https://archivists.org/ns/eac/v2",
-        format=mdf.EAC_CPF,
+        mdt_format=mdf.EAC_CPF,
         version="2.0",
     ),
     MetsMetadataType(
         uri="http://www.lido-schema.org",
-        format=mdf.LIDO,
+        mdt_format=mdf.LIDO,
         version="1.1"
     ),
     MetsMetadataType(
         uri="ddi:instance:3_3",
-        format=mdf.DDI,
+        mdt_format=mdf.DDI,
         version="3.3"),
     MetsMetadataType(
         uri="ddi:instance:3_2",
-        format=mdf.DDI,
+        mdt_format=mdf.DDI,
         version="3.2"),
     MetsMetadataType(
         uri="ddi:instance:3_1",
-        format=mdf.DDI,
+        mdt_format=mdf.DDI,
         version="3.1"),
     MetsMetadataType(
         uri="ddi:codebook:2_5",
-        format=mdf.DDI,
+        mdt_format=mdf.DDI,
         version="2.5.1"),
     MetsMetadataType(
         uri="http://www.icpsr.umich.edu/DDI",
-        format=mdf.DDI,
+        mdt_format=mdf.DDI,
         version="2.1"
     ),
     MetsMetadataType(
         uri="http://www.vraweb.org/vracore4.htm",
-        format=mdf.VRA,
+        mdt_format=mdf.VRA,
         version="4.0"
     ),
     MetsMetadataType(
         uri="http://www.arkivverket.no/standarder/addml",
-        format=mdf.OTHER,
+        mdt_format=mdf.OTHER,
         other_format="ADDML",
         version="8.3",
     ),
     MetsMetadataType(
         uri="http://datacite.org/schema/kernel-4",
-        format=mdf.OTHER,
+        mdt_format=mdf.OTHER,
         other_format="DATACITE",
         version="4.3",
     ),
     MetsMetadataType(
         uri="http://www.loc.gov/audioMD",
-        format=mdf.OTHER,
+        mdt_format=mdf.OTHER,
         other_format="AudioMD",
         version="2.0",
     ),
     MetsMetadataType(
         uri="http://www.loc.gov/videoMD",
-        format=mdf.OTHER,
+        mdt_format=mdf.OTHER,
         other_format="VideoMD",
         version="2.0",
     ),
     MetsMetadataType(
         uri="urn:ebu:metadata-schema:ebucore",
-        format=mdf.OTHER,
+        mdt_format=mdf.OTHER,
         other_format="EBUCORE",
         version="1.10",
     ),
@@ -127,7 +127,7 @@ METS_MDTYPES_LIST = [
 METS_MDTYPES = {
     mdt.uri:
         {
-            'format':       mdt.format,
+            'format':       mdt.mdt_format,
             'other_format':  mdt.other_format,
             'version':      mdt.version
         } for mdt in METS_MDTYPES_LIST
--- tests/metadata/mets_metadata_test.py
+++ tests/metadata/mets_metadata_test.py
@@ -6,11 +6,11 @@ from mets_builder.metadata.metadata import MetadataFormat as mdf
 def test_valid_metadata_without_other_format():
     obj = MetsMetadataType(
         uri="http://purl.org/dc/elements/1.1",
-        format=mdf.DC,
+        mdt_format=mdf.DC,
         version="8.3",
     )
     assert obj.uri == "http://purl.org/dc/elements/1.1"
-    assert obj.format == mdf.DC
+    assert obj.mdt_format == mdf.DC
     assert obj.version == "8.3"
     assert obj.other_format is None
 
@@ -18,11 +18,11 @@ def test_valid_metadata_without_other_format():
 def test_valid_metadata_with_other_format():
     obj = MetsMetadataType(
         uri="http://datacite.org/schema/kernel-4",
-        format=mdf.OTHER,
+        mdt_format=mdf.OTHER,
         other_format="DATACITE",
         version="4.3",
     )
-    assert obj.format == mdf.OTHER
+    assert obj.mdt_format == mdf.OTHER
     assert obj.other_format == "DATACITE"
 
 
@@ -30,7 +30,7 @@ def test_missing_required_fields_raises_assertion():
     with pytest.raises(AssertionError):
         # uri missing
         MetsMetadataType(
-            format=mdf.OTHER,
+            mdt_format=mdf.OTHER,
             other_format="DATACITE",
             version="4.3",
         )
@@ -40,6 +40,6 @@ def test_format_other_without_other_format_raises_value_error():
     with pytest.raises(ValueError):
         MetsMetadataType(
             uri="http://datacite.org/schema/kernel-4",
-            format=mdf.OTHER,
+            mdt_format=mdf.OTHER,
             version="4.3",
         )
